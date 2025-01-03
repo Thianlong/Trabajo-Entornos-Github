d2a3e6422d4adb44de5424f1e816a11c56350904
Melchor <melchorruizgarcia@gmail.com>
2025-01-03 16:17:53 +0100

    Resuelto conflicto en .gitignore y añadido a pyct

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Trabajo-Entornos-Github
 hola paco
 primer cambio en el README.md
+segundo cambio en el README.md
---
+++ src/main/java/com/Melchor/Launcher.java
@@ -0,0 +1,8 @@
+package com.Melchor;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+	}
+}
